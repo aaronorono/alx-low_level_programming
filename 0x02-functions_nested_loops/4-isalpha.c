3e2e5d79ed0cf9faf3dadaedb105d134f1d7f0cf
aaronorono <elvisorono12@gmail.com>
2023-02-21 03:03:50 -0800

    check alphabet character

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,12 +1,11 @@
 #include "main.h"
 
 /**
- * _isapha - fuction that checks for alphabets
+ * _isalpha - checks for alphabetic character
  *
  * @c: parameter to be checked
  *
- * Return: 1 if it is an alphabet
- * and 0 otherwise
+ * Return: 1 if it is an alphabet, and 0 otherwise
  */
 
 int _isalpha(int c)
