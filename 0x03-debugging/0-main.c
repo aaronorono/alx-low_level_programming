8a635d45336ed256ada1919e504fbddeb5b527ab
aaronorono <elvisorono12@gmail.com>
2023-02-22 01:00:30 -0800

    task 0

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,15 +1,15 @@
 #include "main.h"
 
 /**
- * main - Test function for positive or negative
- * Return: Always 0
- */
+* main - tests function that prints if integer is positive or negative
+* Return: 0
+*/
 
 int main(void)
 {
-        int i;
-
-        i = 0;
-        positive_or_negative(i);
-        return (0);
+	int i;
+	
+	i = 0;
+	positive_or_negative(i);
+	return (0);
 }
