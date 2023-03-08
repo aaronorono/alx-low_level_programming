f77d3827315bed91301e733be768bc78a4c68bb8
aaronorono <elvisorono12@gmail.com>
2023-03-08 10:44:01 -0800

    function that prints a string in reverse

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,19 +1,32 @@
 #include "main.h"
 /**
- * _strlen_recursion - Returns the length of a string.
- * @s: The string to be measured.
- *
- * Return: The length of the string.
+ * _puts_recursion - function like puts();
+ * @s: input
+ * Return: Always 0 (Success)
  */
-int _strlen_recursion(char *s)
+void _puts_recursion(char *s)
 {
-	int longit = 0;
-
 	if (*s)
 	{
-		longit++;
-		longit += _strlen_recursion(s + 1);
+		_putchar(*s);
+		_puts_recursion(s + 1);
 	}
 
-	return (longit);
+	else
+		_putchar('\n');
+}
+
+
+#include "main.h"
+/**
+ * _print_rev_recursion - Prints a string in reverse.
+ * @s: The string to be printed.
+ */
+void _print_rev_recursion(char *s)
+{
+	if (*s)
+	{
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
+	}
 }
